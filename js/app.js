93a3851320ec2a37d6221c0a375608b51851e724
VIKAS BAIRWA <71213518+Cyber420-india@users.noreply.github.com>
2022-06-17 18:58:10 +0530

    a new project started

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log('welcome to notes app');
+
+//event listners
+
+//if user  adds a note, add it to the local storage
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];  
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+    // showNotes();
+});
